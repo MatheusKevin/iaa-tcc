61b8e46f3f1a80d4898e461006d348bf0ce7d0be
Matheus Pelegrini <kevin.pelegrini@outlook.com>
2022-03-10 00:20:23 -0300

    conversão de imagem

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import cv2
 import numpy as np
 
 from tensorflow import keras
-from tensorflow.keras.preprocessing import image
+from keras.preprocessing import image
 
 
 DATA_DIR = "C:\\Users\\mathe\\Desktop\\TCC_IAA\\midlevel"
 
 model = keras.models.load_model("modelo.h5")
-
+print(model.summary())
 
 class_names = os.listdir(DATA_DIR)
 
@@ -20,8 +20,8 @@ for class_name in class_names:
         success, img = vidcap.read()
         while success:
             success, img = vidcap.read()
+            img.resize((64, 64, 3))
             xd = image.img_to_array(img)
-            xd = np.expand_dims(xd, axis=-1)
+            xd = np.expand_dims(xd, axis=0)
             deep_features = model.predict(xd)
-
             print(deep_features.shape)
